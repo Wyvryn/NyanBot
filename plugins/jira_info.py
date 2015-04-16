7fe4b4fdb7d8f0aec6e33e35a7976e052bef474a
Mike <wyvryn@gmail.com>
2015-04-16 10:38:22 -0500

    Cleaned up Jira plugin

--- plugins/jira_info.py
+++ plugins/jira_info.py
@@ -3,6 +3,7 @@ from will.decorators import respond_to, periodic, hear, randomly, route, rendere
 from jira.client import JIRA
 import os
 
+
 class JiraPlugin(WillPlugin):
 
     @hear("!jira (?P<case_number>.*)", case_sensitive=False)
@@ -15,8 +16,12 @@ class JiraPlugin(WillPlugin):
             ticket = jira.issue(case_number)
             name = ticket.fields.summary
             status = ticket.fields.status
+            issuetype = ticket.fields.issuetype
             url = "http://jira.texturallc.net/browse/" + case_number
-            resp = str(case_number) + "<br /><a href='" + str(url) + "'>JIRA Link</a><br />Name: " + str(name) + "<br />Status: " + str(status)
+            resp = (str(case_number) + "<br /><a href='" +
+                    str(url) + "'>JIRA Link</a><br />Name: " + str(name) +
+                    "<br />Type: " + str(issuetype) + "<br />Status: " +
+                    str(status))
             self.say(resp, message, html=True)
         except:
             self.say("I couldn't find that.", message)
